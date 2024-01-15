6ee2f8ab425080cd8a744dd088e863dfdbcf015f
vasilisg1989 <v.giannikasnet21@gmail.com>
2024-01-15 16:04:02 +0200

    config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,25 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl:'https://ebepirus.natech.gr/el/Account/UserLogin',
+    specPattern:'cypress/integration/**/*.js',
+    defaultCommandTimeout:20000,
+    reporter:'mochawesome',
+    reporterOptions: {
+      embeddedScreenshots : true,
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true,
+      reportFileName: 'Report',
+    },
+    video:false,
+    projectId: "Cypress-Assignment"
+  },
+  retries: {
+    runMode: 2,
+    openMode: 2
+
   },
 });
